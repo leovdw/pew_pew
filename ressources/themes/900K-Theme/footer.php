73d3998ada44702bb534b551c715fc096003aa58
Leo <leopold.von-der-weid@hetic.net>
2019-06-10 16:34:37 +0200

    Hotjar update

--- ressources/themes/900K-Theme/footer.php
+++ ressources/themes/900K-Theme/footer.php
@@ -1,15 +1,5 @@
 </div>
 <?php wp_footer() ?>
-<!-- Hotjar Tracking Code for http://winkle.fr -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1356909,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+
 </body>
 </html>
