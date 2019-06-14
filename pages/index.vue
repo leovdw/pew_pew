2668ad759c975e3aec6e2931bf405b4bd2994a63
leovdw <l.vdw@hotmail.fr>
2019-06-14 12:37:01 +0200

    Update Debug

--- pages/index.vue
+++ pages/index.vue
@@ -85,7 +85,7 @@
         </svg>
       </div>
 
-      <!-- <div class="has_scroll"> -->
+      <div class="has_scroll">
         <div class="home__blow">
           <transition name="blow" apear>
             <div class="" v-show="this.current >= 1">
@@ -97,7 +97,7 @@
         </div>
       </div>
 
-      <!-- <div class="home__sum home__exp"> -->
+      <div class="home__sum home__exp">
         <div class="home__exp--head">
           <svg width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M10 0H60C65.5229 0 70 4.47715 70 10V60C70 65.5229 65.5229 70 60 70H10C4.47715 70 0 65.5229 0 60V10C0 4.47715 4.47715 0 10 0Z" fill="white"/>
@@ -185,7 +185,7 @@
       </div>
 
       <!-- <div class="home__how"> -->
-        <h2>Comment ça marche ?</h2>
+        <!-- <h2>Comment ça marche ?</h2>
         <ul>
           <li>
             <i>1</i>
@@ -208,10 +208,10 @@
             <p>Un large choix d’activités t’est proposé : sport, loisirs créatifs, musique, loisirs culturels… Afin de te détendre, sortir de ta zone de confort, développer ton potentiel...</p>
           </li>
         </ul>
-      </div>
+      </div> -->
 
       <!-- <div class="home__why"> -->
-        <h2>Pourquoi choisir Winkle ?</h2>
+        <!-- <h2>Pourquoi choisir Winkle ?</h2>
         <p>Parce que...</p>
         <ul>
           <li>
@@ -229,10 +229,10 @@
         <h5>* Signifie gagner en anglais <br>
         ** langage familier signifiant « gagnante »</h5>
 
-      </div>
+      </div> -->
 
       <!-- <div class="home__newsletter"> -->
-        <div class="home__newsletter--form">
+        <!-- <div class="home__newsletter--form">
           <h3>Tu souhaites entendre parler de nous ?</h3>
           <p>Si tu souhaites être tenue au courant des actualités de notre projet, et savoir quand nous le lancerons officiellement, n’hésite pas à laisser ton mail !</p>
           <form name="contact" method="POST" data-netlify="true">
@@ -305,7 +305,7 @@
           </svg>
         </div>
       </div>
-    </div>
+    </div> -->
     <!-- <footer class="footer"> -->
       <div class="footer--logo">
         <svg width="124" height="25" viewBox="0 0 124 25" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
