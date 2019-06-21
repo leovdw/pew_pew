93201f3435a7b5f89c1faaaed44738632ded1278
leovdw <l.vdw@hotmail.fr>
2019-06-21 12:20:33 +0200

    Update cookies stuff

--- pages/index.vue
+++ pages/index.vue
@@ -410,12 +410,25 @@
       </div>
       <div class="footer--legal">
         <a href="https://www.iubenda.com/privacy-policy/54072312" class="iubenda-white iubenda-embed" title="Privacy Policy ">Privacy Policy</a>
-        <script type="text/javascript">
-        (function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
-        </script>
         <a href="#">Mentions légales</a>
         <!-- <nuxt-link to="/leg"></nuxt-link> -->
       </div>
+      <script type="text/javascript">
+      (function (w,d) {
+        var loader = function () {
+          var s = d.createElement("script"),
+          tag = d.getElementsByTagName("script")[0];
+          s.src="https://cdn.iubenda.com/iubenda.js";
+          tag.parentNode.insertBefore(s,tag);};
+          if(w.addEventListener){
+            w.addEventListener("load", loader, false);
+          }else if(w.attachEvent){
+            w.attachEvent("onload", loader);
+          }else{
+            w.onload = loader;
+          }
+        })(window, document);
+      </script>
     </footer>
   </div>
 </template>
