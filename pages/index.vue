12fabd997c9c5ddef8670f4c557b472ad99c60d2
leovdw <l.vdw@hotmail.fr>
2019-06-21 12:22:02 +0200

    Update Cookies

--- pages/index.vue
+++ pages/index.vue
@@ -416,14 +416,14 @@
       <script type="text/javascript">
       (function (w,d) {
         var loader = function () {
-          var s = d.createElement("script"),
-          tag = d.getElementsByTagName("script")[0];
+          var s = d.createElement('script'),
+          tag = d.getElementsByTagName('script')[0];
           s.src="https://cdn.iubenda.com/iubenda.js";
           tag.parentNode.insertBefore(s,tag);};
           if(w.addEventListener){
-            w.addEventListener("load", loader, false);
+            w.addEventListener('load', loader, false);
           }else if(w.attachEvent){
-            w.attachEvent("onload", loader);
+            w.attachEvent('onload', loader);
           }else{
             w.onload = loader;
           }
